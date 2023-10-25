6d60af4858b02bf8933975d140bf1fcc96096156
derrekdev <derrekworking@gmail.com>
2023-10-25 22:17:11 +0800

    updated layout border on search bar

--- src/features/Home/HomeSearch/HomeSearch.tsx
+++ src/features/Home/HomeSearch/HomeSearch.tsx
@@ -19,7 +19,7 @@ export default function HomeSearch() {
     <HomeSectionLayout>
       <div className="flex flex-row">
         <Select>
-          <SelectTrigger className="w-[180px] text-yellow-300 border-r-0 rounded- focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-0 focus:ring-offset-0">
+          <SelectTrigger className="w-[180px] text-yellow-300 border-r-0 rounded-l-md rounded-r-none focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-0 focus:ring-offset-0">
             <SelectValue  placeholder="Select a fruit" />
           </SelectTrigger>
           <SelectContent>
@@ -34,7 +34,7 @@ export default function HomeSearch() {
             </SelectGroup>
           </SelectContent>
         </Select>
-        <Input type="text" className="text-yellow-300 focus-visible:ring-0 focus-visible:ring-offset-0 border-l-0" placeholder="Search your movie here..." />
+        <Input type="text" className="text-yellow-300 focus-visible:ring-0 focus-visible:ring-offset-0 border-l-0 rounded-r-md rounded-l-none" placeholder="Search your movie here..." />
       </div>
     </HomeSectionLayout>
   );
